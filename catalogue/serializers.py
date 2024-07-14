0ba15a867dbc8c4ee61fe20f0aafa0607de6a35c
affaxerd1 <caffaxerd@kabarak.ac.ke>
2024-07-14 04:26:53 +0300

    how serializers work

--- catalogue/serializers.py
+++ catalogue/serializers.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Artist, Album, Song
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -18,14 +19,42 @@ class Comment:
         self.created = created or datetime.now()
 
 
-class CommentSerializer(serializers.Serializer):
-    email = serializers.EmailField()
-    content = serializers.CharField(max_length=200)
-    created = serializers.DateTimeField()
+class ArtistSerializer(serializers.ModelSerializer):
+    """ Automatically sets a pk field (ex : id :1"""
 
-    def create(self, validated_data):
-        return Comment(**validated_data)
+    class Meta:
+        model = Artist
+        fields = "__all__"
 
-    def update(self, instance, validated_data):
-        instance.email = validated_data.get('email', instance)
-        instance.email = validated_data.get('email', instance)
+
+# class ArtistHyperlinkedSerializer(serializers.HyperlinkedModelSerializer):
+#     """ Sets a url field instead of a pk field(ex: url: 'http://api.example.com/user/1"""
+#
+#     class Meta:
+#         model = Artist
+#         fields = "__all__"
+
+
+class AlbumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Album
+        fields = "__all__"
+
+
+class SongSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Song
+        fields = "__all__"
+
+## serializer overview
+# class CommentSerializer(serializers.Serializer):
+#     email = serializers.EmailField()
+#     content = serializers.CharField(max_length=200)
+#     created = serializers.DateTimeField()
+#
+#     def create(self, validated_data):
+#         return Comment(**validated_data)
+#
+#     def update(self, instance, validated_data):
+#         instance.email = validated_data.get('email', instance)
+#         instance.email = validated_data.get('email', instance)
