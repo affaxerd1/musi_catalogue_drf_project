33e199eda878848da67bbebd238f571f6f511f10
affaxerd1 <caffaxerd@kabarak.ac.ke>
2024-07-17 01:08:16 +0300

    function views

--- catalogue/views.py
+++ catalogue/views.py
@@ -2,6 +2,9 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework import permissions
 from catalogue.serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, throttle_classes
+from rest_framework.throttling import UserRateThrottle
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -11,3 +14,12 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.AllowAny]
+
+class OncePerDayUserThrottle(UserRateThrottle):
+    rate = '1/day'
+@api_view(['GET', 'POST'])
+@throttle_classes([OncePerDayUserThrottle])
+def hello_world(request):
+    if request.method == 'POST':
+        return Response({'message' : 'got some data :' + str(request.data)} )
+    return Response({'message':'hello world!'})
